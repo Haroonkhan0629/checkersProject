39d3f07e0c6dbb2fbf7eb8609ba605bcb3b88431
Haroon Khan <hronkkhan6@gmail.com>
2023-10-09 15:54:00 -0700

    new hangman project

---
+++ script.js
@@ -0,0 +1,14 @@
+let allWords = ["word", "spear", "fish"]
+
+let gameWord = document.getElementById("game-word")
+
+let randomWord = allWords[Math.floor(Math.random() * allWords.length)]
+
+splitWord = randomWord.split(" ")
+
+splitWord.forEach(function() {
+    
+})
+
+
+
