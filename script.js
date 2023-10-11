4b73ceff885380083be7e2e52dd0d37c83212797
Haroon Khan <hronkkhan6@gmail.com>
2023-10-11 01:22:50 -0700

    tuesday night

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ randomWord = allWords[Math.floor(Math.random() * allWords.length)]
 
 splitWord = randomWord.split("")
 
-function startGame() {
-    gameLetters = []
-    arr = []
-    for (i = 0; i < splitWord.length; i++) {
-        gameLetters.push(splitWord[i])
-        splitWord[i] = "_"
-        arr.push(splitWord[i])
-        document.getElementById("game-word").textContent = arr
+gameWord = document.getElementById("game-word")
+gameLetters = []
+blanks = []
 
-        // document.getElementById("game-word").addEventListener("click", function() {
-        //     guessedLetter = prompt("Guess a letter")
-        // })
-        // if(guessedLetter.includes(gameLetters[i])) {
-        //     // make guessed letter appear in correct place
-        // }
-    }
+for (i = 0; i < splitWord.length; i++) {
+    gameLetters.push(splitWord[i])
+    splitWord[i] = "_"
+    blanks.push(splitWord[i])
+    gameWord.textContent = blanks
 }
+
+gameWord.addEventListener("click", function () {
+    guessedLetter = prompt("Guess a letter")
+    if (gameLetters.includes(guessedLetter) === true) {
+        for (i = 0; i < gameLetters.length; i++) {
+            if (gameLetters[i] === guessedLetter) {
+                blanks[i] = guessedLetter
+                gameWord.textContent = blanks
+            }
+        }
+    } else if (gameLetters.includes(guessedLetter) === false) {
+        alert("wrong")
+    }
+})
+
 
 
 
